d429e53d6fbe60c51786472aebb58151098a3ff6
krrsalmane <karrounsalmane@gmail.com>
2024-12-30 09:18:29 +0100

    calculate intrest in savings account

--- src/CurrentAccount.java
+++ src/CurrentAccount.java
@@ -1,46 +1,58 @@
+
+
 import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class CurrentAccount extends Account {
     private String bank_fees;
     static ArrayList<CurrentAccount>currentAccounts = new ArrayList<CurrentAccount>();
 
-    public CurrentAccount(int number, double balance, Client owner) {
-        super(number,balance,owner);
+
+    public CurrentAccount(int number, double balance, Client owner, String bank_fees) {
+        super(number, balance, owner);
+        this.bank_fees = bank_fees;
     }
+
 
     public CurrentAccount(){
         super();
     }
 
+
     public String getBank_fees() {
         return bank_fees;
     }
 
+
     public void setBank_fees(String bank_fees) {
         this.bank_fees = bank_fees;
     }
-    public void CurentAccount(){
-        System.out.println("enter client Id");
+    public void CurentAccount() {
+        System.out.println("Enter client ID:");
         int clientId = sc.nextInt();
-        sc.nextLine();
-        Client client= RechercheClientIdExiste(clientId);
-        if (client != null){
-            System.out.println("enter account number");
-            int account_number = sc.nextInt();
-            System.out.println("enter how much money you have");
-            double balance = sc.nextDouble();
-            try {
-                balance = Double.parseDouble(sc.nextLine());
-            } catch (NumberFormatException e) {
-                System.out.println("Balance invalide. You should enter a valide  number.");
-                return;
-            }
-            CurrentAccount Ca = new CurrentAccount(account_number,balance,client);
-            currentAccounts.add(Ca);
-        }else {
-            System.out.println("You should add a Client");
-        }
+        sc.nextLine(); // Consume newline character
+        Client client = RechercheClientIdExiste(clientId);
 
-    }
-}
+
+        if (client != null) {
+            System.out.println("Enter account number:");
+            int account_number = sc.nextInt();
+            System.out.println("Enter your balance:");
+            double balance = sc.nextDouble();
+            sc.nextLine(); // Consume newline character
+
+
+            System.out.println("Enter bank fees:");
+            String bank_fees = sc.nextLine();
+
+
+            // Create and add the CurrentAccount object
+            CurrentAccount ca = new CurrentAccount(account_number, balance, client, bank_fees);
+            currentAccounts.add(ca);
+            System.out.println("Current account added successfully!");
+        } else {
+            System.out.println("You should add a Client first.");
+        }
+    }}
+
--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,49 +1,58 @@
+
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class SavingsAccount extends Account {
-    private double interest_rate;
-    static ArrayList<SavingsAccount>savingsAccounts= new ArrayList<>();
 
-    public SavingsAccount(double interest_rate) {
+public class CurrentAccount extends Account {
+    private String bank_fees;
+    static ArrayList<CurrentAccount>currentAccounts = new ArrayList<CurrentAccount>();
+
+
+    public CurrentAccount(int number, double balance, Client owner, String bank_fees) {
+        super(number, balance, owner);
+        this.bank_fees = bank_fees;
+    }
+
+
+    public CurrentAccount(){
         super();
-        this.interest_rate = interest_rate;
     }
 
-    public SavingsAccount(int number,double balance,Client owner){
-        super(number,balance,owner);
-    }
-    public SavingsAccount(){}
 
-    public double getInterest_rate() {
-        return interest_rate;
+    public String getBank_fees() {
+        return bank_fees;
     }
 
-    public void setInterest_rate(double interest_rate) {
-        this.interest_rate = interest_rate;
+
+    public void setBank_fees(String bank_fees) {
+        this.bank_fees = bank_fees;
     }
-
-    public void  Saving_Account(){
-
-        System.out.println("enter client Id");
+    public void CurentAccount() {
+        System.out.println("Enter client ID:");
         int clientId = sc.nextInt();
-        sc.nextLine();
-        Client client= RechercheClientIdExiste(clientId);
-      if (client != null){
-          System.out.println("enter account number");
-          int account_number = sc.nextInt();
-          System.out.println("enter how much money you have");
-          double balance = sc.nextDouble();
-          SavingsAccount Sa = new SavingsAccount(account_number,balance,client);
-          savingsAccounts.add(Sa);
-      }else {
-          System.out.println("You should add a Client");
-      }
-    }
-    public void Calculate_Intrest(){
-        System.out.println("enter interest rate a year ");
-        double balance = getBalance();
-        double interest_rate  =(balance*3)/100;
-    }
-    }
+        sc.nextLine(); // Consume newline character
+        Client client = RechercheClientIdExiste(clientId);
+
+
+        if (client != null) {
+            System.out.println("Enter account number:");
+            int account_number = sc.nextInt();
+            System.out.println("Enter your balance:");
+            double balance = sc.nextDouble();
+            sc.nextLine(); // Consume newline character
+
+
+            System.out.println("Enter bank fees:");
+            String bank_fees = sc.nextLine();
+
+
+            // Create and add the CurrentAccount object
+            CurrentAccount ca = new CurrentAccount(account_number, balance, client, bank_fees);
+            currentAccounts.add(ca);
+            System.out.println("Current account added successfully!");
+        } else {
+            System.out.println("You should add a Client first.");
+        }
+    }}
 
